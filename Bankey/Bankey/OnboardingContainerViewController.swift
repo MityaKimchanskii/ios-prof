a68f2b023eaf09fbac03feeb45c082bfdfd94b3c
Dmitrii Kim <mitya_kim@icloud.com>
2022-08-08 13:14:44 -0700

    refactor: create generic onboarding viewController
    This commit adds a generic onboarding viewController that con be reused across multiple onboarding screens.

--- Bankey/Bankey/OnboardingContainerViewController.swift
+++ Bankey/Bankey/OnboardingContainerViewController.swift
@@ -22,9 +22,9 @@ class OnboardingContainerViewController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
+        let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and a brand new look and feel that will make you feel lile you are back in the 1989.")
+        let page2 = OnboardingViewController(heroImageName: "world", titleText: "Move your money around the world quickly and securely.")
+        let page3 = OnboardingViewController(heroImageName: "thumbs", titleText: "Learn more at www.bankey.com")
         
         pages.append(page1)
         pages.append(page2)
@@ -95,26 +95,3 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
         return pages[index + 1]
     }
 }
-
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-    }
-}
-
--- Bankey/Bankey/OnboardingViewController.swift
+++ Bankey/Bankey/OnboardingViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    // MARK: - Properties
+    let heroImageName: String
+    let titleText: String
+    
     // MARK: - Views
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
+    
+    // MARK: - Init
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -28,6 +44,7 @@ class OnboardingViewController: UIViewController {
 extension OnboardingViewController {
     
     func style() {
+        view.backgroundColor = .systemBackground
         
         // StackView
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -37,7 +54,7 @@ extension OnboardingViewController {
         // ImageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -45,7 +62,7 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
     
     func layout() {
