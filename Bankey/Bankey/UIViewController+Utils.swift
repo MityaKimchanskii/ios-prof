bd2f39112f004efcef66dc1f10db29aabcfd89ba
Dmitrii Kim <mitya_kim@icloud.com>
2022-08-08 22:29:22 -0700

    TabBarController commit

--- Bankey/Bankey/UIViewController+Utils.swift
+++ Bankey/Bankey/UIViewController+Utils.swift
@@ -10,12 +10,11 @@ import UIKit
 
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusBarView = UIView(frame: frame)
-        
-        statusBarView.backgroundColor = appColor
-        view.addSubview(statusBarView)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
     
     func setTabBarImage(imageName: String, title: String) {
