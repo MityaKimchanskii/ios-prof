3b4d824fad1ee6494a8ca62053f1c1005abde12b
Dmitrii Kim <mitya_kim@icloud.com>
2022-08-08 21:17:12 -0700

    feature: Add TabBar

---
+++ Bankey/Bankey/MainViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MainViewController.swift
+//  Bankey
+//
+//  Created by Mitya Kim on 8/8/22.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupTabBar()
+    }
+    
+    private func setupViews() {
+        let summaryVC = AccountSummaryViewController()
+        let moneyVC = MoveMoneyViewController()
+        let moreVC = MoreViewController()
+        
+        summaryVC.setTabBarImage(imageName: "list.dash.header.rectangle", title: "Summary")
+        moneyVC.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Move Money")
+        moreVC.setTabBarImage(imageName: "ellipsis.circle", title: "More")
+        
+        let summaryNC = UINavigationController(rootViewController: summaryVC)
+        let moneyNC = UINavigationController(rootViewController: moneyVC)
+        let moreNC = UINavigationController(rootViewController: moreVC)
+        
+        summaryNC.navigationBar.barTintColor = appColor
+        hideNavigationBarLine(summaryNC.navigationBar)
+        
+        let tabBarList = [summaryNC, moneyNC, moreNC]
+        
+        viewControllers = tabBarList
+    }
+    
+    private func hideNavigationBarLine(_ navigationBar: UINavigationBar) {
+        let img = UIImage()
+        navigationBar.shadowImage = img
+        navigationBar.setBackgroundImage(img, for: .default)
+        navigationBar.isTranslucent = false
+    }
+    
+    private func setupTabBar() {
+        tabBar.tintColor = appColor
+        tabBar.isTranslucent = false
+    }
+}
+
+class AccountSummaryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class MoveMoneyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
+class MoreViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
---
+++ Bankey/Bankey/UIViewController+Utils.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Mitya Kim on 8/8/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let statusBarSize = UIApplication.shared.statusBarFrame.size
+        let frame = CGRect(origin: .zero, size: statusBarSize)
+        let statusBarView = UIView(frame: frame)
+        
+        statusBarView.backgroundColor = appColor
+        view.addSubview(statusBarView)
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
