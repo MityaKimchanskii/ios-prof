be95cc5c15c47a021862914c971509cf61b51495
Dmitrii Kim <mitya_kim@icloud.com>
2022-08-11 01:05:36 -0700

    Add Unit Tests for Network

---
+++ Bankey/BankeyUnitTests/AccountSummary/ProfileTests.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileTests.swift
+//  BankeyUnitTests
+//
+//  Created by Mitya Kim on 8/11/22.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
